7cc30f70b645f23dd18ae1b63ca934e22c4df0cf
imwisdom <33623136+imwisdom@users.noreply.github.com>
2019-03-26 09:23:03 +0000

    addProject (#11)

---
+++ IdeaProjects/src/main/java/com/riotgames/kr/account/App.java
@@ -0,0 +1,13 @@
+package com.riotgames.kr.account;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
