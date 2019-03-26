df256e91b887603b5609d33ac1f62d6de4f1ca13
wisdom <kjihye0340@naver.com>
2019-03-26 19:01:35 +0900

    one commit

--- IdeaProjects/src/main/java/com/riotgames/kr/account/ImPerson.java
+++ IdeaProjects/src/main/java/com/riotgames/kr/account/ImPerson.java
@@ -8,5 +8,21 @@ public class ImPerson {
     private String name;
     private int age;
 
+    public int getAge() {
+        return age;
+    }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void print()
+    {
+        System.out.print(name+" "+age);
+    }
 }
---
+++ IdeaProjects/src/main/java/com/riotgames/kr/account/main.java
@@ -0,0 +1,14 @@
+package com.riotgames.kr.account;
+
+public class main {
+
+    public static void main(String[] args)
+    {
+        ImPerson a = new ImPerson();
+        a.setAge(12);
+        a.setName("jihye");
+        a.print();
+    }
+
+}
+
